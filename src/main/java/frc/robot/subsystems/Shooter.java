2d3fa6f1d92cf7cea05f7e0efa8b24cada603ee8
GuyEmmanuel <pat.sosinowski@gmail.com>
2025-11-19 16:11:09 -0800

    new motor system
    Removed unused variables and configurations for Motion Magic settings in the Shooter subsystem.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,40 +8,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.ShooterConstants;
 
-public class Shooter extends SubsystemBase{
-  
-  private static final int SHOOTER_MOTOR_ID = 6;
-  private static final String CAN_BUS = "canivore"; // or "rio"
-    public final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
-// set target position to 100 rotations
-
-  private final TalonFX m_talonFX = new TalonFX(SHOOTER_MOTOR_ID, CAN_BUS); 
 public Shooter() {
 
   var talonFXConfigs = new TalonFXConfiguration();
 //(just look up on websites for) 
   var slot0Configs = talonFXConfigs.Slot0; 
-slot0Configs.kS = 0.25; // Add 0.25 V output to overcome static friction
-slot0Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
-slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
 slot0Configs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
 slot0Configs.kI = 0; // no output for integrated error
 slot0Configs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
-
-
-  // set Motion Magic settings
-var motionMagicConfigs = talonFXConfigs.MotionMagic;
-motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
-motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
-motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
-
 m_talonFX.getConfigurator().apply(talonFXConfigs);
-
-
-
-
-//final MotionMagicVoltage m_request = new MotionMagicVoltage(0);
-
+//addded
 }
 public void moveToPosition(double rotations) {
   
